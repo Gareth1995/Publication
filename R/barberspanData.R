69ac4806063f5bd6d2e091fb1989aaf0f9a3c371
Gareth Edwards <gareth.reeve50@gmail.com>
2023-06-21 16:33:55 +0200

    Results code completed

--- R/barberspanData.R
+++ R/barberspanData.R
@@ -18,16 +18,18 @@ getCwacSiteInfo(site_id)
 
 # get barberspan counts
 bp_counts2 <- getCwacSiteCounts(site_id)
-bp_counts2
+bp_counts2 <- bp_counts2 %>% mutate(names = paste(Common_species, Common_group))
 
 # convert to wide format (cols, bird names, season, year)
-wider_df = bp_counts2 %>% pivot_wider(names_from = SppRef, values_from = Count)
+wider_df = bp_counts2 %>% pivot_wider(names_from = names, values_from = Count)
 info_df = wider_df %>% select(c('Year', 'Season'))
 
-bird_counts = wider_df %>% select('262':'1016')
+bird_counts = wider_df %>% select('Marsh Sandpiper':'Mallard NA')
 
 # bind info onto bird_counts
-bird_counts = cbind(info_df, bird_counts)
+all_barberspan_counts = cbind(info_df, bird_counts)
+# save all bird counts
+save(all_barberspan_counts, file = "data/all_Barberspan_counts.RData")
 
 # count number of nas in each column
 na_counts = colSums(is.na(bird_counts))
@@ -35,6 +37,7 @@ na_counts = colSums(is.na(bird_counts))
 # get bird ids that contain more than 19 counts
 valid_cols = names(na_counts[(nrow(bird_counts) - na_counts) > 19])
 barberspan_counts = bird_counts[valid_cols]
+
 save(barberspan_counts, file = "data/Barberspan_counts.RData")
 
 
